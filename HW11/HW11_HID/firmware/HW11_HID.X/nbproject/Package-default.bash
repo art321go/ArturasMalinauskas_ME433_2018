33eaffd60374afe814d6b7b09ec2a180ebe996f0
Arturas <34588990+art321go@users.noreply.github.com>
2018-05-31 17:46:02 -0500

    Starting HW11

--- HW11/HW11_HID/firmware/HW11_HID.X/nbproject/Package-default.bash
+++ HW11/HW11_HID/firmware/HW11_HID.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/HDI_HW11.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=HDI_HW11.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=hdihw11.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/HW11_HID.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=HW11_HID.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=hw11hid.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/hdihw11.x/bin
+makeDirectory ${TMPDIR}/hw11hid.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/hdihw11.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/hw11hid.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/hdihw11.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/hw11hid.x.tar *
 checkReturnCode
 
 # Cleanup
